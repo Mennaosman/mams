0beb4f21977efc2c4bcae2a5a79a6782a2350536
Mennaosman <61330554+Mennaosman@users.noreply.github.com>
2020-10-08 22:19:42 +0200

    Add files via upload

---
+++ laboratory.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+    <form method="POST">
+    	xray center name :<input type="text" name="xray center"\><br>
+		examination type :<input type="text" name="examination type"\><br>
+		examination cost :<input type="text" name="examination cost"\><br>
+		time table :<input type="text" name="time table"\><br>
+        address:<input type="text" name="adress"\><br>
+		
+    </form>
+
+</body>
+</html>
